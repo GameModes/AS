c3b3ea7a054d330f1a3f08615f1fe3a80d61880f
rubenski0345@gmail.com <Bocuzawekon.99>
2021-11-23 09:06:18 +0100

    basic messages

---
+++ magicmaze/main.py
@@ -0,0 +1,11 @@
+class maze:
+    def __init__(self, test):
+        self.test = test
+
+class agent:
+    def __init__(self, test):
+        self.test = test
+
+class policy:
+    def __init__(self, test):
+        self.test = test
